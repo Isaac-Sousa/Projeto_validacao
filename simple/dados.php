ffee353f4399620e8c2c6807aa23b15739a71a95
Isaac-Sousa <isaacsousa200@gmail.com>
2022-08-24 16:12:21 -0300

    retornando dados

--- simple/dados.php
+++ simple/dados.php
@@ -6,17 +6,12 @@
 <?php
 $nome = $_POST['f_nome'];
 $senha = $_POST["f_senha"];
-if ($senha == "Dev21" && $nome == "Isaac") {
-  echo "<h2> <script>alert('OLÁ, seu user é valido')</script> </h2> <br>";
-} elseif ($senha == "Dev21" && $nome != "Isaac") {
-  echo "<h2> <script>alert('Usuário ou senha inválidos')</script> </h2> <br>";
-  echo '<script>window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "/naofoi.php";</script>';
-} elseif ($senha = " " && $nome = " ") {
-  echo "<h2> <script>alert('Usuário inexistente')</script> </h2> <br>";
-  echo "<h2> <script>alert('Logue com um usuário existente!')</script> </h2> <br>";
-  echo '<script>window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "/naofoi.php";</script>';
-} else {
-  echo "<h2> <script>alert('Aguardando as informações')</script> </h2> <br>";
+ require_once("naosei.php");
+ if($nome == "Isaac" && $senha == "Dev21" ){
+    echo  "<script>alert('Você está autorizado')</script> <br>"; 
+    echo "<script>alert('Seus dados confidenciais são:')</script>"."<br>";
+    echo "Quase no pirei quando quebrei, e tu mostrou que era algo simples"."<br>";
+    echo "O código está como queria no começo"."<br>";
 }
 ?>
 </body>
