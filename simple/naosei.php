a2850848fc990496f13a283b7bff94c01a80d7bf
pedrosaints <pedro.santos@fixpay.com>
2022-08-24 15:28:49 -0300

    ajuste redirect

--- simple/naosei.php
+++ simple/naosei.php
@@ -14,13 +14,11 @@ if ($senha == "Dev21" && $nome == "Isaac") {
   echo "<h2> <script>alert('OLÁ, seu user é valido')</script> </h2> <br>";
 } elseif ($senha == "Dev21" && $nome != "Isaac") {
   echo "<h2> <script>alert('Usuário ou senha inválidos')</script> </h2> <br>";
-} elseif ($senha != "Dev21" && $nome == "Isaac") {
-  echo "<h2> <script>alert('Usuário ou senha inválidos')</script> </h2> <br>";
-  echo '<script>window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "/simple/naofoi.php";</script>';
+  echo '<script>window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "/naofoi.php";</script>';
 } elseif ($senha = " " && $nome = " ") {
   echo "<h2> <script>alert('Usuário inexistente')</script> </h2> <br>";
   echo "<h2> <script>alert('Logue com um usuário existente!')</script> </h2> <br>";
-  echo '<script>window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "/simple/naofoi.php";</script>';
+  echo '<script>window.location.href = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "/naofoi.php";</script>';
 } else {
   echo "<h2> <script>alert('Aguardando as informações')</script> </h2> <br>";
 }
